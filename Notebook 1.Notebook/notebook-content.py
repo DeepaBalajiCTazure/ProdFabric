47cdb1874f15523ed5b1603e752c6121d5d51f74
DeepaBalajiCTazure <149655012+DeepaBalajiCTazure@users.noreply.github.com>
2026-07-06 09:48:50 +0530

    Committing 18 items from workspace ddd7637e-f578-4439-b0d0-5a872019ac54

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "3bcbbb25-74ae-4576-aaa7-0ea3e65c0ac4",
+# META       "default_lakehouse_name": "lhforDFgen2",
+# META       "default_lakehouse_workspace_id": "ddd7637e-f578-4439-b0d0-5a872019ac54",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "3bcbbb25-74ae-4576-aaa7-0ea3e65c0ac4"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
